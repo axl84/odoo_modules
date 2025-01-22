9177116ed12cf2b1447e330cea51db17e981cddd
Oleg Usenko <oleg911usenko@gmail.com>
2025-01-22 12:45:19 +0200

    add new module oleg_weba_footbik

---
+++ oleg_weba_footbik/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "oleg_weba_footbik",
+    "summary": """
+        """,
+    "author": "Oleg (Weba)",
+    "website": "https://weba.com.ua/",
+    "category": "Uncategorized",
+    "version": "17.0.1.0.0",
+    "depends": ["base"],
+    "data": [
+        # "security/ir.model.access.csv",
+    ],
+}
